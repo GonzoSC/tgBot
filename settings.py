e56db452e0ab0cfe0fef8dcd687f9d7c419110b5
GonzoSC <54544012+GonzoSC@users.noreply.github.com>
2019-08-26 20:34:55 +0500

    Version 1.0

---
+++ settings.py
@@ -0,0 +1,9 @@
+from urllib3 import make_headers
+REQUEST_KWARGS={
+    'proxy_url': 'http://185.221.161.186:9573',
+    'urllib3_proxy_kwargs': {
+        'proxy_headers': make_headers(proxy_basic_auth='RDVGq8:sypHbt')
+                            }
+                }
+
+API_KEY = "911611551:AAEhkAQPoueOzwZJfe4N8YtC4OXpFGgltiw"
